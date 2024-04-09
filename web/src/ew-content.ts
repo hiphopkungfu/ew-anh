7954d87e3434c4783ac952e23fcfdb66fff31f4e
Anh <quang.a.bui@gmail.com>
2024-04-09 17:35:24 +0200

    refactor: remove Hello world

--- web/src/ew-content.ts
+++ web/src/ew-content.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from "../node_modules/lit";
-import { customElement, property } from "../node_modules/lit/decorators";
+import { customElement } from "../node_modules/lit/decorators";
 import sharedStyles from "./sharedStyles";
 import "./file-selector.js";
 import "./ew-button.js";
@@ -20,30 +20,12 @@ export class EwContent extends LitElement {
     `,
   ];
 
-  @property()
-  world = "";
-
   render() {
     return html`
       <div class="container center">
-        <h1>Hello, ${this.world}</h1>
-
         <file-selector></file-selector>
         <ew-button></ew-button>
       </div>
     `;
-  }
-
-  async _getWorld() {
-    const response = await fetch("/api/hello");
-    if (!response.ok) {
-      const text = await response.text();
-      throw new Error(
-        `Request rejected with status ${response.status} and message ${text} `
-      );
-    }
-    const jsonData = await response.json();
-    const resp = JSON.parse(jsonData);
-    this.world = resp.data;
   }
 }
