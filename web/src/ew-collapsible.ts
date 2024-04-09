3712cfc5de26919c668503d50d963d94296817cd
Anh <quang.a.bui@gmail.com>
2024-04-09 21:48:13 +0200

    feature: display answers on cards
    Aligned placeholder elements on card

--- web/src/ew-collapsible.ts
+++ web/src/ew-collapsible.ts
@@ -7,7 +7,7 @@ export class EwCollapsible extends LitElement {
   static styles = [
     sharedStyles,
     css`
-      .collapsible-container {
+      .container {
         display: flex;
         flex-direction: column;
         width: 100%;
@@ -15,13 +15,24 @@ export class EwCollapsible extends LitElement {
         border: 1px solid var(--ew-gray-light);
         border-radius: var(--input-border-radius);
       }
-
+      .content {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        padding: 2em;
+      }
+      .header {
+        border-bottom: 1px solid var(--ew-gray-light);
+      }
       .answers {
         flex: 1;
+        align-self: flex-start;
+        text-align: left;
       }
-
       .footer {
+        display: flex;
         justify-self: flex-end;
+        flex-direction: row;
       }
     `,
   ];
@@ -43,31 +54,36 @@ export class EwCollapsible extends LitElement {
   stronglyDisagree = 0;
 
   render() {
-    return html`<div class="collapsible-container">
+    return html`<div class="container">
       <div class="header">
-        <h1>${this.title}</h1>
+        <h3>${this.title}</h1>
       </div>
 
-      <div class="answers">
-        <div>agree: ${this.agree}</div>
-        <div>disagree ${this.disagree}</div>
-        <div>neutral ${this.neutral}</div>
-        <div>stronglyAgree ${this.stronglyAgree}</div>
-        <div>stronglyDisagree ${this.stronglyDisagree}</div>
-      </div>
-
-      <div class="footer">
-        <div>
-          [answered]
-          ${this.agree +
-          this.disagree +
-          this.neutral +
-          this.stronglyAgree +
-          this.stronglyDisagree}
+      <div class="content">
+        <div class="answers">
+            <div>agree: ${this.agree}</div>
+            <div>disagree ${this.disagree}</div>
+            <div>neutral ${this.neutral}</div>
+            <div>stronglyAgree ${this.stronglyAgree}</div>
+            <div>stronglyDisagree ${this.stronglyDisagree}</div>
         </div>
 
-        <div>[skipped] ${this.skipped}</div>
+        <div class="footer">
+            <div>
+            [answered]
+            ${
+              this.agree +
+              this.disagree +
+              this.neutral +
+              this.stronglyAgree +
+              this.stronglyDisagree
+            }
+            </div>
+
+            <div>[skipped] ${this.skipped}</div>
+        </div>
       </div>
+
     </div>`;
   }
 }
